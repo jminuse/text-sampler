d98758e3a6b1df1be5bcdc2f3e816822d434b0be
James Stevenson <2008714+jminuse@users.noreply.github.com>
2023-03-02 19:25:42 -0500

    Less memory use via better use of sparse matrices

--- hamlet.py
+++ hamlet.py
@@ -1,15 +1,18 @@
 '''
-Simple generative text model based on Hamlet
+Simple generative text model based on Shakespeare
 '''
 import numpy as np
-from scipy.sparse import csr_array, dok_array
+from scipy.sparse import dok_array
+from tqdm import tqdm
 
-with open('hamlet.txt') as f:
-    ok_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?!.'[]"
-    ok_chars += ok_chars.lower()
-    text = ''.join([c if c in ok_chars else ' ' for c in f.read()])
+# https://ocw.mit.edu/ans7870/6/6.006/s08/lecturenotes/files/t8.shakespeare.txt
+with open('shakespeare.txt') as f:
+    text = f.read()
+ok_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?!.'[]"
+ok_chars += ok_chars.lower()
+text = ''.join([c if c in ok_chars else ' ' for c in text])
 
-input_N = 1
+input_N = 2  # input_N = 3 tends to simply quote
 tokens = text.split()
 inputs = [' '.join(tokens[i:i + input_N]) for i in range(0, len(tokens) - input_N)]  # all input contexts
 inputs_by_ind = list(sorted(set(inputs)))
@@ -20,27 +23,32 @@ print('Unique outputs:', len(outputs_by_ind))
 counts = dok_array((len(inputs_by_ind), len(outputs_by_ind)), dtype=np.float32)
 inds_by_input = {w:i for i, w in enumerate(inputs_by_ind)}
 inds_by_output = {w:i for i, w in enumerate(outputs_by_ind)}
+totals_by_input_ind = np.zeros(len(inputs_by_ind))
+print('First and last inputs:', inputs_by_ind[:3], inputs_by_ind[-3:])
 
-for i in range(len(tokens) - input_N):
+for i in tqdm(range(len(tokens) - input_N)):
     input_context = ' '.join(tokens[i:i + input_N])
     next_output = tokens[i + input_N]
     input_index = inds_by_input[input_context]
     output_index = inds_by_output[next_output]
     counts[input_index, output_index] += 1
+    totals_by_input_ind[input_index] += 1
 
-# norms = csr_array(np.sum(counts, axis=1).reshape(-1, 1))
-norms = np.sum(counts, axis=1).reshape(-1, 1)
-# print(f'{norms.shape = }')
-# print(f'{counts.shape = }')
-sample_probs = counts / norms # or try https://stackoverflow.com/questions/16043299/substitute-for-numpy-broadcasting-using-scipy-sparse-csc-matrix
-sample_probs_uniform = (counts != 0.0).todense().astype(np.float32)  # make dense, sadly
-#sample_probs_uniform = counts != 0.0
+print(f'{counts.shape = }')
+print(f'{counts.count_nonzero() = }')
 
-print(sample_probs.shape)
-#print('sparsity = ', 1 - sample_probs.count_nonzero() / sample_probs.size)
-print(inputs_by_ind[:3], inputs_by_ind[-3:])
-print(sample_probs_uniform.shape)
-#print('sparsity = ', 1 - sample_probs_uniform.count_nonzero() / sample_probs_uniform.size)
+sample_probs = dok_array((len(inputs_by_ind), len(outputs_by_ind)), dtype=np.float32)
+count_nonzero_by_input_ind = np.zeros(len(inputs_by_ind))
+for input_index, output_index in tqdm(zip(*counts.nonzero())):
+    sample_probs[input_index, output_index] = counts[input_index, output_index] / totals_by_input_ind[input_index]
+    count_nonzero_by_input_ind[input_index] += 1
+    
+sample_probs_uniform = dok_array((len(inputs_by_ind), len(outputs_by_ind)), dtype=np.float32)
+for input_index, output_index in tqdm(zip(*counts.nonzero())):
+    sample_probs_uniform[input_index, output_index] = 1.0 / count_nonzero_by_input_ind[input_index]
+
+print(f'{sample_probs.shape = }')
+print(f'{sample_probs.count_nonzero() = }')
 
 output = []
 seed = np.random.randint(10**8)
@@ -52,33 +60,17 @@ sample_N = 200
 for step in range(sample_N):
     input_context = ' '.join(all_outputs[-input_N:])
     # print(input_context)
+    input_index = [inds_by_input[input_context]]
     if np.random.random() < 0.5:  # choose by probability
-        p = sample_probs[inds_by_input[input_context]]
-    else:  # choose any nonzero probability equally
-        p = sample_probs_uniform[inds_by_input[input_context], :]
-    p /= p.sum()
+        p = sample_probs[input_index, :]
+    else:  # choose any nonzero probability equally (often will switch topics)
+        p = sample_probs_uniform[input_index, :]
+    p = p.todense().flatten()  # assuming np.random.choice wants dense input
+    p /= p.sum()  # make sure probabilities are fully normalized
     new_output = np.random.choice(outputs_by_ind, p=p)
     all_outputs.append(new_output)
-for i, word in enumerate(all_outputs):
-    if word == word.upper():  # all-uppercase indicates speaker directions
-        all_outputs[i] = '\n' + word  # add line breaks
-# also add line breaks at the end of sentences
+# add line breaks at the end of sentences
 text_out = ' '.join(all_outputs).replace('. ', '.\n').replace('! ', '!\n')
 print('=' * 40)
 print(text_out)
 print('=' * 40)
-
-'''
-Fun examples:
-seed=39308240, input_N=1
-eaten a farm it please you good friends!
-How now this play? QUEEN GERTRUDE Nay then That we do pall and is't my story.
-March within my bed.
-KING CLAUDIUS [Aside] How now a robustious periwig pated fellow might please you Or is king dunks to pass through our bad begins and the pregnant sometimes march? by himself.
-KING CLAUDIUS How now!
-LAERTES The canker galls the proud revengeful ambitious with all believe it.
-ROSENCRANTZ Most generous thoughts That he suffer this rude against our sides and an I see.
-Takes the ability of love passing well.
-OPHELIA No fairy takes
-
-'''
